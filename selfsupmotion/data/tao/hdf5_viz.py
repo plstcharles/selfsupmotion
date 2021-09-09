17f68fa86c1733fb8b3503bb8e0d13128703d4b6
Raphael Jean <raphael.jean@rocketmail.com>
2021-09-09 19:50:46 -0400

    Arg parser + Unique random color per tid

--- selfsupmotion/data/tao/hdf5_viz.py
+++ selfsupmotion/data/tao/hdf5_viz.py
@@ -6,14 +6,15 @@ import cv2 as cv
 import selfsupmotion.data.tao.data_module as tao_data_module
 
 logger = logging.getLogger(__name__)
+import argparse
 
 
-def main():
-    data_root_path = "/nfs/server/datasets/tao/"
+def main(args):
+
     import_only_annot_frames = True
 
     data_module = tao_data_module.TAODataModule(
-        data_root_path=data_root_path,
+        data_root_path=args.data_root_path,
         use_hdf5_packages=True,
         import_only_annot_frames=import_only_annot_frames,
         skip_private_datasets=True,
@@ -34,24 +35,43 @@ def main():
                 image = frame_dict["image_data"]
                 got_gt = False
                 for tid, track_info in frame_dict["tracks"].items():
+                    import random
+
+                    rnd = lambda: random.randint(0, 255)
+                    random.seed(int(tid))
+                    r, g, b = rnd(), rnd(), rnd()
+                    # print("#%02X%02X%02X" % (r(), r(), r()))
                     if track_info["annotation"]:
                         bbox = track_info["annotation"]["bbox"]
                         cv.rectangle(
                             image,
                             pt1=(bbox[0], bbox[1]),
                             pt2=(bbox[0] + bbox[2], bbox[1] + bbox[3]),
-                            color=(112, 112, 255),
+                            color=(r, g, b),
                             thickness=1,
                         )
                         got_gt = True
-                image = cv.resize(image, dsize=(-1, -1), fx=4, fy=4, interpolation=cv.INTER_NEAREST)
+                image = cv.resize(
+                    image,
+                    dsize=(-1, -1),
+                    fx=args.fx,
+                    fy=args.fy,
+                    interpolation=cv.INTER_NEAREST,
+                )
                 cv.imshow("test", image)
                 cv.waitKey(0 if got_gt else 1)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Vis tool for TAO")
+    parser.add_argument(
+        "--data-root-path", type=str, default="/nfs/server/datasets/tao/"
+    )
+    parser.add_argument("--fx", type=int, default=4)
+    parser.add_argument("--fy", type=int, default=4)
+    args = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
-    main()
+    main(args)
     print("all done")
 
     # we want to highlight how much better representations are when using temporal-pairs instead of synthetic-aug-pairs
